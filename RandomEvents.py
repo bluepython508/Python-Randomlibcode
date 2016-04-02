6aa131658b14fdfc7488715bfa33718b4fc2ed37
bluepython508 <ben@probook.local>
2016-04-02 14:09:29 +0100

    added multi-dice support and made a neater printing system

--- RandomEvents.py
+++ RandomEvents.py
@@ -1,18 +1,34 @@
 import random
+import time
+prompt = ">>>"
+
+def string_from_list(list_name):
+    string = ''''''
+    for x in range(len(list_name)):
+        tempstring = '''roll ''' + str(x + 1) + ''': ''' + str(list_name[x-1]) + '''
+'''
+        string = string + tempstring
+    return(string)
 def flipcoin():
     flip = random.random()
+    time.sleep(0.5)
     if flip > 0.5:
         return("heads")
     elif flip < 0.5:
         return("tails")
     else:
-        return("invalid")
+        return("The coin escaped")
 
 def roll():
-    return random.randint(1, 6)
+    n = int(input("How many dice would you like to roll?"))
+    rolled = []
+    for x in range(n):
+        rolled.append(random.randint(1,6))
+        time.sleep(1)
+    return(string_from_list(rolled))
 
-def main():
-    keyboardinput = input()
+def loop():
+    keyboardinput = input(prompt)
     if keyboardinput:
         if keyboardinput == "flip":
             return(flipcoin())
@@ -20,6 +36,8 @@ def main():
             return(roll())
         elif keyboardinput == "stop":
             return("Goodbye")
+        else:
+            return("I don't understand")
 
 def welcome():
     print("Random Event Progam:")
@@ -27,12 +45,15 @@ def welcome():
     print("Type flip to flip a coin.")
     print("Type roll dice to roll a die")
     print("Type stop to quit the program.")
-           
 
-if __name__ == "__main__":
+def main():
     welcome()
     while True:
-        returned = main()
+        returned = loop()
         print(returned)
         if returned == "Goodbye":
             break
+           
+
+if __name__ == "__main__":
+    main()
