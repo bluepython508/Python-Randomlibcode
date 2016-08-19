54ee8664c273a13dbe729dd35fc00d76cdc56a24
Your Name <you@example.com>
2016-08-19 11:44:48 +0100

    Change lib used, clean up generally, add help

--- GUI.py
+++ GUI.py
@@ -1,50 +1,45 @@
+#!/bin/python3
 from RandomEvents import *
 from tkinter import *
-tk = Tk()
-tk.title("Random Event Generator")
-canvas = Canvas(tk, width=1000, height=700)
-canvas.grid(row=1, column=0)
-frame = Frame(tk)
-frame.grid(row=2, column=0)
-canvas.create_text(500, 50, text='Event Generator', fill="red" )
-canvas.create_text(500, 70, text='-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------', fill="green" )
-def update(string):
-    messagebox.showinfo("Event", string)
-def rolled():
-    tk2 = Tk()
-    #tk.wait_window(tk2)
-    tk2.title("dice")
-    
-    def close():
-        try:
-            number = int(num.get())
-            tk2.destroy()
-            if number < 1 or number > 15:
-                number = 1
-            results = roll(number)
-            update(results)
-        except ValueError:
-            messagebox.showerror("Error", "Try Again")
-    num = Entry(tk2)
-    num.bind("<Return>", close)
-    label = Label(tk2, text="How many dice?")
-    done = Button(tk2, text="Done",command=close)
-    done.pack(side=BOTTOM)
-    num.pack(side=RIGHT)
-    label.pack(side=LEFT)
-    tk2.mainloop()
-def flip():
-    results = flipcoin()
-    update(results)
-def quitprog():
-    if messagebox.askokcancel(title="QUIT", message="Are you sure?"):
-        tk.destroy()
-quitbutton = Button(frame, text="QUIT", fg='red',bg='green', command=quitprog)
-flipbutton = Button(frame, text="Flip a Coin", command=flip)
-rollbutton = Button(frame, text="Roll some Dice", command=rolled)
-flipbutton.grid(row=0, column=0)
-rollbutton.grid(row=0, column=2)
-quitbutton.grid(row=0, column=1)
-tk.mainloop()
+from pyautogui import *
 
-    
+def update(string):
+	alert(text=string, title="Event", button='OK')
+def rolled():
+	try:
+		number = int(prompt(text="How many dice would you like to roll?(1-15)", title="Dice"))
+		if number or number == 0:
+			if number < 1 or number > 15:
+				number = 1
+			results = roll(number)
+			update(results)
+		else:
+			alert(title="Error", text="ERROR: Please enter a number between 1 and 15", button="OK")
+	except ValueError:
+		alert(title="Error", text="ERROR: Please enter a number between 1 and 15", button='OK')
+def flip():
+	results = flipcoin()
+	update(results)
+def quitprog():
+	if confirm(title="QUIT", text="Are you sure?", buttons=['Quit','Cancel'])== "Quit":
+		tk.destroy()
+
+def main():
+	global tk
+	tk = Tk()
+	tk.title("Random Event Generator")
+	canvas = Canvas(tk, width=1000, height=700)
+	canvas.grid(row=1, column=0)
+	frame = Frame(tk)
+	frame.grid(row=2, column=0)
+	canvas.create_text(500, 50, text='Random Event Generator', fill="red" )
+	canvas.create_text(500, 70, text='-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------', fill="green" )
+	quitbutton = Button(frame, text="QUIT", fg='red',bg='green', command=quitprog)
+	flipbutton = Button(frame, text="Flip a Coin", command=flip)
+	rollbutton = Button(frame, text="Roll some Dice", command=rolled)
+	flipbutton.grid(row=0, column=0)
+	rollbutton.grid(row=0, column=2)
+	quitbutton.grid(row=0, column=5)
+	tk.mainloop()
+if __name__ == "__main__":
+	main()
--- RandomEvents.py
+++ RandomEvents.py
@@ -1,59 +1,63 @@
 import random
 import time
+import math
 prompt = "READY>"
 
 def string_from_list(list_name):
-    string = ''''''
-    for x in range(len(list_name)):
-        tempstring = '''roll ''' + str(x + 1) + ''': ''' + str(list_name[x-1]) + '''
-'''
-        string = string + tempstring
-    return(string)
+	string = ''''''
+	for x in range(len(list_name)):
+		tempstring = '''Roll ''' + str(x + 1) + ''': '''str(list_name[x-1]) + '''\n'''
+		string = string + tempstring
+	return(string)
 def flipcoin():
-    flip = random.random()
-    time.sleep(0.5)
-    if flip > 0.5:
-        return("heads")
-    elif flip < 0.5:
-        return("tails")
-    else:
-        return("The coin escaped")
+	flip = random.random()
+	time.sleep(0.5)
+	if flip > 0.5:
+		return("Heads")
+	elif flip < 0.5:
+		return("Tails")
+	else:
+		return("The coin escaped")
 
 def roll(n):
-    rolled = []
-    for x in range(n):
-        rolled.append(random.randint(1,6))
-        time.sleep(1)
-    return(string_from_list(rolled))
+	rolled = []
+	for x in range(n):
+		rolled.append(random.randint(1,6))
+		#time.sleep(1)
+	return(string_from_list(rolled))
 
 def loop():
-    keyboardinput = input(prompt)
-    if keyboardinput:
-        if keyboardinput == "flip":
-            return(flipcoin())
-        elif keyboardinput =="roll dice":
-            n = int(input("How many dice would you like to roll?"))
-            return(roll(n))
-        elif keyboardinput == "stop":
-            return("Goodbye")
-        else:
-            return("I don't understand")
+	keyboardinput = input(prompt)
+	if keyboardinput:
+		if keyboardinput == "flip":
+			return(flipcoin())
+		elif keyboardinput =="roll dice":
+			n = int(round(float(input("How many dice would you like to roll?"))))
+			return(roll(n))
+		elif keyboardinput == "stop":
+			return("Goodbye")
+		elif keyboardinput == "help":
+			welcome()
+			return
+		else:
+			return("I don't understand")
 
 def welcome():
-    print("Random Event Progam:")
-    print("----------------------")
-    print("Type flip to flip a coin.")
-    print("Type roll dice to roll a die")
-    print("Type stop to quit the program.")
+	print("Random Event Progam:")
+	print("----------------------")
+	print("Type flip to flip a coin.")
+	print("Type roll dice to roll a dice")
+	print("Type help to show this help message.")
+	print("Type stop to quit the program.")
 
 def main():
-    welcome()
-    while True:
-        returned = loop()
-        print(returned)
-        if returned == "Goodbye":
-            break
-           
+	welcome()
+	while True:
+		returned = loop()
+		print(returned)
+		if returned == "Goodbye":
+			break
+		   
 
 if __name__ == "__main__":
-    main()
+	main()
